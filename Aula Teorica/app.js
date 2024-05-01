e9e33e298c3a337153aac0e4869cb9c5568226e2
Andre Vieira <andrevieira19992018@gmail.com>
2024-04-30 21:28:46 -0300

    criando funções

--- Aula Teorica/app.js
+++ Aula Teorica/app.js
@@ -1,9 +1,18 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+/* let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto'; */
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 0 e 10';
+/* let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 0 e 10'; */
 
 function verificarChute(){
     console.log("O botão chute foi clicado!");
 }
+
+function exibirTextoNaTela(tag,texto){
+    let  titulo = document.querySelector(tag);
+    titulo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1','Jogo do número secreto');
+exibirTextoNaTela('p','Escolha um número entre 0 e 100');
+
